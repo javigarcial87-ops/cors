671770925e7db0d2ec95698cc7eb39513d7c0a25
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-13 18:43:19 +0100

    coger personajes

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const url = 'https://rickandmortyapi.com/api/character'
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const URL = "https://rickandmortyapi.com/api/character"
 
 
+app.get("/characters", async(req, res)=>{
+    try {
+        const response = await axios.get(URL)
+        res.json(response.data.results)
+        
+    } catch(error) {
+        res.status(500).json({
+            error:"Error al obtener los personajes"
+        })
+    }
+})
 
 
-
-
+app.listen (4000, ()=>{
+    console.log('express esta funcionando en el puerto http://localhost:4000')
+})
 
 
 
